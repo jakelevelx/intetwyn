4299bbcbe8ae1e769db056eed39898319df55444
Sam Kolb <samuel.kolb@gmail.com>
2014-02-15 11:28:36 -0800

    added login functionality

--- cloud/app.js
+++ cloud/app.js
@@ -1,35 +1,73 @@
 
-// These two lines are required to initialize Express in Cloud Code.
-var express = require('express');
-var app = express();
+  var express = require('express');
+  var parseExpressHttpsRedirect = require('parse-express-https-redirect');
+  var parseExpressCookieSession = require('parse-express-cookie-session');
+  var app = express();
 
-// Global app configuration section
-app.set('views', 'cloud/views');  // Specify the folder to find templates
-app.set('view engine', 'ejs');    // Set the template engine
-app.use(express.bodyParser());    // Middleware for reading request body
+  app.set('views', 'cloud/views');
+  app.set('view engine', 'ejs');
+  app.use(parseExpressHttpsRedirect());  // Require user to be on HTTPS.
+  app.use(express.bodyParser());
+  app.use(express.cookieParser('YOUR_SIGNING_SECRET'));
+  app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
 
-// This is an example of hooking up a request handler with a specific request
-// path and HTTP verb using the Express routing API.
-app.get('/hello', function(req, res) {
-  console.log('get handler');
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
-});
-app.post('/hello', function(req, res) {
-  console.log('post handler for form ' + req.body.message);
-  res.render('hello', { message: req.body.message });
-});
+  // You could have a "Log In" link on your website pointing to this.
+  app.get('/login', function(req, res) {
+    // Renders the login form asking for username and password.
+    res.render('login.ejs');
+  });
 
-// // Example reading from the request query string of an HTTP get request.
-// app.get('/test', function(req, res) {
-//   // GET http://example.parseapp.com/test?message=hello
-//   res.send(req.query.message);
-// });
+  // Clicking submit on the login form triggers this.
+  app.post('/login', function(req, res) {
+    Parse.User.logIn(req.body.username, req.body.password).then(function() {
+      // Login succeeded, redirect to homepage.
+      // parseExpressCookieSession will automatically set cookie.
+      res.redirect('/');
+    },
+    function(error) {
+      // Login failed, redirect back to login form.
+      res.redirect('/login');
+    });
+  });
 
-// // Example reading from the request body of an HTTP post request.
-// app.post('/test', function(req, res) {
-//   // POST http://example.parseapp.com/test (with request body "message=hello")
-//   res.send(req.body.message);
-// });
+  // You could have a "Log Out" link on your website pointing to this.
+  app.get('/logout', function(req, res) {
+    Parse.User.logOut();
+    res.redirect('/');
+  });
 
-// Attach the Express app to Cloud Code.
-app.listen();
+  // The homepage renders differently depending on whether user is logged in.
+  app.get('/', function(req, res) {
+    if (Parse.User.current()) {
+      // No need to fetch the current user for querying Note objects.
+      var Note = Parse.Object.extend("Note");
+      var query = new Parse.Query(Note);
+      query.find().then(function(results) {
+        // Render the notes that the current user is allowed to see.
+      },
+      function(error) {
+        // Render error page.
+      });
+    } else {
+      res.redirect('/login');
+    }
+  });
+
+  // You could have a "Profile" link on your website pointing to this.
+  app.get('/profile', function(req, res) {
+    // Display the user profile if user is logged in.
+    if (Parse.User.current()) {
+      // We need to fetch because we need to show fields on the user object.
+      Parse.User.current().fetch().then(function(user) {
+        // Render the user profile information (e.g. email, phone, etc).
+      },
+      function(error) {
+        // Render error page.
+      });
+    } else {
+      // User not logged in, redirect to login form.
+      res.redirect('/login');
+    }
+  });
+
+  app.listen();
